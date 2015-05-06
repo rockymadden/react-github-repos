1d01d773e31e150cc9d131bf84b6aa4242957df5
Rocky Madden <git@rockymadden.com>
2015-05-06 00:41:46 -0600

    Const and arrow functions.

--- __tests__/GitHubRepos.js
+++ __tests__/GitHubRepos.js
@@ -1,14 +1,14 @@
 jest.dontMock('../src/GitHubRepos.js');
 
-var GitHubRepos = require('../src/GitHubRepos');
-var React = require('react/addons');
-var TestUtils = React.addons.TestUtils;
+const GitHubRepos = require('../src/GitHubRepos');
+const React = require('react/addons');
+const TestUtils = React.addons.TestUtils;
 
-// TODO: Expansion of tests and handling for browser globals over requires.
-describe('GitHubRepos', function() {
-  it('initial render should work', function() {
-    var repos = TestUtils.renderIntoDocument(<GitHubRepos username="rockymadden" />);
-    var ul = TestUtils.findRenderedDOMComponentWithTag(repos, 'ul').getDOMNode();
+// TODO: Expansion of tests.
+describe('GitHubRepos', () => {
+  it('initial render should work', () => {
+    const repos = TestUtils.renderIntoDocument(<GitHubRepos username="rockymadden"/>);
+    const ul = TestUtils.findRenderedDOMComponentWithTag(repos, 'ul').getDOMNode();
 
     expect(ul.getAttribute('class')).toEqual('githubrepos');
   });
